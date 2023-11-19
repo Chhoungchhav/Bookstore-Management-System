081039200f128d75b29b98db207b1b370195141b
Chhav <ungchhounngchhav@gmail.com>
2023-11-19 22:43:07 +0700

    merge book.java from branch2

--- Book.java
+++ Book.java
@@ -1,11 +1,28 @@
 package Bookstore_Management;
 
 public class Book {
-    private int bookId, publicationYear; 
-    private String title, isbn, genre, authorName;
-    private double salePrice, importPrice;
-
-    public void setBookTitle(){
-
+    private int bookID, instockbook;
+    private String title;
+    private double saleprice, importprice;
+    private Date importDate;
+    Book(int bookID, String title, double saleprice, double importprice, int instockbook, Date importDate){
+        this.bookID = bookID;
+        this.title = title;
+        this.saleprice = saleprice;
+        this.importDate = importDate;
+        this.importprice = importprice;
+        this.instockbook = instockbook;
     }
+    
+    public int getBookID(int bookID) {
+        return bookID;
+    }
+    public int getStock(int stock){
+        return instockbook;
+    }
+    public double getPrice(int price){
+        return saleprice;
+    }
+
+
 }
