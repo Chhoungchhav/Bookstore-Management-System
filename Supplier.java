86dc7f65d421c25819a346a55e4470a47bfa7b49
Chhav <ungchhounngchhav@gmail.com>
2023-11-19 22:37:18 +0700

    Add constructor

--- Supplier.java
+++ Supplier.java
@@ -1,6 +1,12 @@
 package Bookstore_Management;
 
 public class Supplier {
-    private int supplierId;
-    private String name, emailAddress, address, genre[];
+    private int supplierID, phoneNumber;
+    private String name, genre[];
+
+    Supplier(int supplierID, String name, int phoneNumber){
+        this.supplierID = supplierID;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
