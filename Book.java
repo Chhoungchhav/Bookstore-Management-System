572b0703f23cdee6216772545440d663e2fc1d49
Takaguya <106542482+Takaguya@users.noreply.github.com>
2023-11-19 22:22:47 +0700

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,11 +1,28 @@
 package Bookstore_Management;
 
 public class Book {
-    private int bookId, publicationYear; 
-    private String title, isbn, genre, authorName;
-    private double salePrice, importPrice;
-
-    public void setBookTitle(){
-
+    private int bookID, instockbook;
+    private String title;
+    private double saleprice, importprice;
+    private Date importDate;
+    public Book(int bookID, String title, double saleprice, double importprice, int instockbook, Date importDate){
+        this.bookID = bookID;
+        this.title = title;
+        this.saleprice = saleprice;
+        this.importDate = importDate;
+        this.importprice = importprice;
+        this.instockbook = instockbook;
     }
+    
+    public static int getBookID(int bookID) {
+        return bookID;
+    }
+    public static int getStock(int stock){
+        return instockbook;
+    }
+    public static double getPrice(int price){
+        return saleprice;
+    }
+
+
 }
